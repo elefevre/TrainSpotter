ae81bee1b8bb2f3f9be4f43636f5c1f93dc8f9d0
Eric Lefevre-Ardant <eric.lefevre@algodeal.com>
2011-04-20 11:46:37 +0200

    Added a real Selenium test

--- play/app/controllers/TrainSpotter.java
+++ play/app/controllers/TrainSpotter.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import play.*;
+import play.data.validation.Required;
 import play.mvc.*;
 
 import java.util.*;
@@ -9,7 +10,12 @@ import models.*;
 
 public class TrainSpotter extends Controller {
 
-	public static void displayTrainDetails(String trainNumber) {
+	public static void displayTrainDetails(@Required String trainNumber) {
+		if (validation.hasErrors()) {
+			flash.error("Please enter the train number");
+			Application.index();
+		}
+
 		render(trainNumber);
 	}
-}
+} 
