c1185fe845d2a98a0d1929e2b90763a5e4bba631
Eric Lefevre-Ardant <eric.lefevre@algodeal.com>
2011-04-21 14:31:12 +0200

    Really uses the parser from the front end

--- play/app/controllers/TrainSpotter.java
+++ play/app/controllers/TrainSpotter.java
@@ -3,19 +3,26 @@ package controllers;
 import play.*;
 import play.data.validation.Required;
 import play.mvc.*;
-
+import java.io.IOException;
+import java.net.MalformedURLException;
 import java.util.*;
-
-import models.*;
+import net.ericlefevre.ResultsPageParser;
+import net.ericlefevre.StatusPageRetriever;
+import net.ericlefevre.URLConnectionProvider;
+import org.joda.time.DateTime;
 
 public class TrainSpotter extends Controller {
-
-	public static void displayTrainDetails(@Required String trainNumber) {
+	public static void displayTrainDetails(@Required String trainNumber) throws MalformedURLException, IOException {
 		if (validation.hasErrors()) {
 			flash.error("Please enter the train number");
 			Application.index();
 		}
 
-		render(trainNumber);
+		DateTime today = new DateTime();
+		
+		String pageContent = new StatusPageRetriever(new URLConnectionProvider()).downloadStatusPageForTrain(trainNumber, today.getYear(), today.getMonthOfYear(), today.getDayOfMonth());
+		
+		ResultsPageParser results = new ResultsPageParser(pageContent);
+		render(results, pageContent);
 	}
-} 
+}
