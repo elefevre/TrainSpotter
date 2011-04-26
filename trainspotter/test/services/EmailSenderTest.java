d1d6285de9eb4b72f6c9c126956d514ee0b1c0cb
Eric Lefevre-Ardant <eric.lefevre@algodeal.com>
2011-04-26 11:52:52 +0200

    Finalized tests

--- trainspotter/test/services/EmailSenderTest.java
+++ trainspotter/test/services/EmailSenderTest.java
@@ -13,7 +13,6 @@ import play.test.UnitTest;
 import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
 import com.amazonaws.services.simpleemail.model.*;
 import com.google.common.collect.Lists;
-import controllers.TrainSpotter;
 
 public class EmailSenderTest extends UnitTest {
 	private final SystemManager mockSystemManager = mock(SystemManager.class);
@@ -39,10 +38,12 @@ public class EmailSenderTest extends UnitTest {
 
 	@Test
 	public void shouldSendEmails() throws MessagingException {
+		Session session = session("aws", "accessKey", "secretKey");
+
 		when(mockSystemManager.getProperty("AWS_ACCESS_KEY_ID")).thenReturn("accessKey");
 		when(mockSystemManager.getProperty("AWS_SECRET_KEY")).thenReturn("secretKey");
 		when(mockAmazonSimpleEmailServiceProvider.getService("accessKey", "secretKey")).thenReturn(mockAmazonSimpleEmailService);
-		when(mockAmazonSimpleEmailServiceProvider.getTransport(sessionWithProperties("aws", "accessKey", "secretKey"))).thenReturn(mockTransport);
+		when(mockAmazonSimpleEmailServiceProvider.getTransport(sessionEqualTo(session))).thenReturn(mockTransport);
 		when(mockAmazonSimpleEmailService.listVerifiedEmailAddresses()).thenReturn(mockListVerifiedEmailAddressesResult);
 		when(mockListVerifiedEmailAddressesResult.getVerifiedEmailAddresses()).thenReturn(Lists.<String> newArrayList("ericlef@gmail.com"));
 
@@ -51,19 +52,21 @@ public class EmailSenderTest extends UnitTest {
 		verify(mockAmazonSimpleEmailService, never()).verifyEmailAddress(mockVerifyEmailAddressRequest);
 		InOrder inOrder = inOrder(mockTransport);
 		inOrder.verify(mockTransport).connect();
-		Message msg = new MimeMessage(session("aws", "accessKey", "secretKey"));
-		msg.setFrom(new InternetAddress("ericlef@gmail.com"));
-		msg.addRecipient(Message.RecipientType.TO, new InternetAddress("ericlef@gmail.com"));
-		msg.setSubject(TrainSpotter.SUBJECT);
-		msg.setText(TrainSpotter.BODY);
-		msg.saveChanges();
-		verify(mockTransport).sendMessage((Message) argThat(new ReflectionEquals(msg, "dh", "contentStream", "headers", "session")), (Address[]) isNull());
-		inOrder.verify(mockTransport).sendMessage(msg, null);
+		inOrder.verify(mockTransport).sendMessage(mimeMessage("ericlef@gmail.com", "ericlef@gmail.com", "Hello World!", "Hello World!"), (Address[]) isNull());
 		inOrder.verify(mockTransport).close();
 	}
 
-	private static Session sessionWithProperties(String transportProtocol, String accessKey, String secretKey) {
-		Session session = session(transportProtocol, accessKey, secretKey);
+	private static Message mimeMessage(String from, String to, String title, String body) throws AddressException, MessagingException {
+		Message msg = new MimeMessage(Session.getInstance(new Properties()));
+		msg.setFrom(new InternetAddress(from));
+		msg.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+		msg.setSubject(title);
+		msg.setText(body);
+		msg.saveChanges();
+		return (Message) argThat(new ReflectionEquals(msg, "dh", "contentStream", "headers", "session")); // would be nice to test session too
+	}
+
+	private static Session sessionEqualTo(Session session) {
 		return (Session) argThat(new ReflectionEquals(session, "providers", "providersByProtocol", "providersByClassName"));
 	}
 
