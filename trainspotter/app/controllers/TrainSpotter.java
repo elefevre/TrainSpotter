a38a9a7426f0c6666f31ebb1b4c8c6a7d0f5be3d
Eric Lefevre-Ardant <eric.lefevre@algodeal.com>
2011-04-26 18:32:35 +0200

    Started using guice

--- trainspotter/app/controllers/TrainSpotter.java
+++ trainspotter/app/controllers/TrainSpotter.java
@@ -3,6 +3,7 @@ package controllers;
 import static play.data.validation.Validation.*;
 import java.io.IOException;
 import java.net.MalformedURLException;
+import javax.inject.Inject;
 import models.TrainInformationPage;
 import org.joda.time.DateTime;
 import play.data.validation.Required;
@@ -11,6 +12,18 @@ import services.*;
 
 public class TrainSpotter extends Controller {
 	private static final String TO = "ericlef@gmail.com";
+	@Inject
+	static SystemManager systemManager;
+	@Inject
+	static TrainInformationPageDao trainInformationPageDao;
+	@Inject
+	static AmazonSimpleEmailServiceProvider amazonSimpleEmailServiceProvider;
+	@Inject
+	static URLConnectionProvider urlConnectionProvider;
+	@Inject
+	static ResultsPageParser resultsPageParser;
+	@Inject
+	static StatusPageRetriever statusPageRetriever;
 
 	public static void displayTrainDetails(@Required String trainNumber) throws MalformedURLException, IOException {
 		if (hasErrors()) {
@@ -27,14 +40,14 @@ public class TrainSpotter extends Controller {
 	}
 
 	private static TrainStatusNotifier getTrainStatusNotifier() {
-		return new TrainStatusNotifier(getEmailSender(), new TrainInformationPageDao());
+		return new TrainStatusNotifier(getEmailSender(), trainInformationPageDao);
 	}
 
 	private static EmailSender getEmailSender() {
-		return new EmailSender(new SystemManager(), new AmazonSimpleEmailServiceProvider());
+		return new EmailSender(systemManager, amazonSimpleEmailServiceProvider);
 	}
 
 	private static StatusPageRetriever getStatusPageRetriever() {
-		return new StatusPageRetriever(new URLConnectionProvider(), new ResultsPageParser());
+		return new StatusPageRetriever(urlConnectionProvider, resultsPageParser);
 	}
 }
