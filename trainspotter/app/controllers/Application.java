9c61080cfadbf65250e4855957129992062b7733
Eric Lefevre-Ardant <eric.lefevre@algodeal.com>
2011-05-09 18:15:36 +0200

    Previous list of users not necessary anymore

--- trainspotter/app/controllers/Application.java
+++ trainspotter/app/controllers/Application.java
@@ -1,14 +1,11 @@
 package controllers;
 
-import java.util.List;
-import models.User;
 import play.mvc.Controller;
 
 public class Application extends Controller {
 
 	public static void index() {
-		@SuppressWarnings("static-access") List<User> users = User.findAll();
-		render(users);
+		render();
 	}
 
 }
