5fbdfda6a771995505dd59cc60c55257771c55e0
Eric Lefevre-Ardant <eric.lefevre@algodeal.com>
2011-05-10 11:52:06 +0200

    Refactoring

--- trainspotter/app/services/StatusPageRetriever.java
+++ trainspotter/app/services/StatusPageRetriever.java
@@ -2,8 +2,10 @@ package services;
 
 import java.io.*;
 import java.net.MalformedURLException;
-
+import java.util.List;
 import models.TrainInformationPage;
+import org.apache.commons.io.IOUtils;
+import com.google.common.base.Joiner;
 
 public class StatusPageRetriever {
 	private final URLConnectionProvider urlConnectionProvider;
@@ -15,19 +17,23 @@ public class StatusPageRetriever {
 	}
 
 	public TrainInformationPage downloadStatusPageForTrain(String trainNumber, int year, int month, int day) throws MalformedURLException, IOException {
-		String inputLine;
-		String result = "";
-
 		BufferedReader inputStreamForUrl = urlConnectionProvider.getInputStreamForUrl(createUrl(trainNumber, year, month, day));
-		while ((inputLine = inputStreamForUrl.readLine()) != null) {
-			result += inputLine + "\n";
-		}
-		return resultsPageParser.toTrainInformationPage(result);
+
+		String results = toMultiLineString(inputStreamForUrl);
+
+		return resultsPageParser.toTrainInformationPage(results);
 	}
 
-	private String createUrl(String trainNumber, int year, int month, int day) {
+	private static String toMultiLineString(BufferedReader inputStreamForUrl) throws IOException {
+		List<String> readLines = IOUtils.readLines(inputStreamForUrl);
+
+		return Joiner.on('\n').join(readLines);
+	}
+
+	private static String createUrl(String trainNumber, int year, int month, int day) {
 		String monthAsString = (month < 10 ? "0" : "") + month;
 		String dayAsString = (day < 10 ? "0" : "") + day;
+
 		return "http://www.infolignes.com/recherche.php?date_num_train=" + year + "|" + monthAsString + "|" + dayAsString + "&num_train=" + trainNumber;
 	}
 
