f846484b2df3923617d9cc0a06c7d74e06fd1f38
Eric Lefevre-Ardant <eric.lefevre@algodeal.com>
2011-05-11 17:36:54 +0200

    More reliable test

--- trainspotter/test/services/StatusPageRetrieverTest.java
+++ trainspotter/test/services/StatusPageRetrieverTest.java
@@ -10,16 +10,15 @@ import play.test.UnitTest;
 
 public class StatusPageRetrieverTest extends UnitTest {
 	private final URLConnectionProvider urlConnectionProvider = mock(URLConnectionProvider.class);
-	private final BufferedReader mockBufferedReader = mock(BufferedReader.class);
 	private final ResultsPageParser mockResultsPageParser = mock(ResultsPageParser.class);
 	private final TrainInformationPage mockTrainInformationPage = mock(TrainInformationPage.class);
 	private final User mockUser = mock(User.class);
 
 	@Test
 	public void shouldObtainTheStatusPageForAGivenTrainAndDay() throws MalformedURLException, IOException {
-		when(urlConnectionProvider.getInputStreamForUrl("http://www.infolignes.com/recherche.php?date_num_train=2011|04|08&num_train=7015")).thenReturn(mockBufferedReader);
-		when(mockBufferedReader.readLine()).thenReturn("line1").thenReturn("line2").thenReturn(null);
-		when(mockResultsPageParser.toTrainInformationPage("line1\nline2\n", mockUser)).thenReturn(mockTrainInformationPage);
+		BufferedReader bufferedReader = new BufferedReader(new StringReader("line1\nline2\n"));
+		when(urlConnectionProvider.getInputStreamForUrl("http://www.infolignes.com/recherche.php?date_num_train=2011|04|08&num_train=7015")).thenReturn(bufferedReader);
+		when(mockResultsPageParser.toTrainInformationPage("line1\nline2", mockUser)).thenReturn(mockTrainInformationPage);
 
 		TrainInformationPage results = new StatusPageRetriever(urlConnectionProvider, mockResultsPageParser).downloadStatusPageForTrain("7015", 2011, 4, 8, mockUser);
 
